b7068f71cc6b993c30b92d29eebefea141db0ced
JonatSrG <JonatSrG>
2022-08-16 22:31:27 -0500

    Templates Jonathan

--- idgs9A/core/views.py
+++ idgs9A/core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
